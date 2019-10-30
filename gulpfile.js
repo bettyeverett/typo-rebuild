3b9e7f9439255c9febed023559025f30719e660a
Bethany Everett <bethanyeeverett@gmail.com>
2019-10-30 16:54:15 +0000

    Installing gulp

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
+let concat = require('gulp-concat')
+let uglify = require('gulp-uglify-es').default;
